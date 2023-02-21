f0c9094dcb065227f6b271e7e4304225411a338d
Harjot Gill <harjot.gill@fluxninja.com>
2023-02-21 14:32:52 -0800

    fix tags parsing

--- codescan/schema.go
+++ codescan/schema.go
@@ -1152,17 +1152,65 @@ func addGoTags(ve *spec.VendorExtensible, field *ast.Field) {
 		return
 	}
 	if strings.TrimSpace(tv) != "" {
-		// split tv into a map of key/value pairs
-		for _, tag := range strings.Split(tv, " ") {
-			// split into key/value pair
-			kv := strings.Split(tag, ":")
-			if len(kv) == 2 {
-				// remove the quotes from the value
-				kv[1] = strings.Trim(kv[1], "\"")
-				addExtension(ve, "x-go-tag-"+kv[0], kv[1])
-			}
+		st := reflect.StructTag(tv)
+		tags := parseStructTag(st)
+		for k, v := range tags {
+			addExtension(ve, "x-go-tag-"+k, v)
 		}
 	}
+}
+
+func parseStructTag(tag reflect.StructTag) map[string]string {
+	tagMap := make(map[string]string)
+
+	for tag != "" {
+		// Skip leading space.
+		i := 0
+		for i < len(tag) && tag[i] == ' ' {
+			i++
+		}
+		tag = tag[i:]
+		if tag == "" {
+			break
+		}
+
+		// Scan to colon. A space, a quote or a control character is a syntax error.
+		// Strictly speaking, control chars include the range [0x7f, 0x9f], not just
+		// [0x00, 0x1f], but in practice, we ignore the multi-byte control characters
+		// as it is simpler to inspect the tag's bytes than the tag's runes.
+		i = 0
+		for i < len(tag) && tag[i] > ' ' && tag[i] != ':' && tag[i] != '"' && tag[i] != 0x7f {
+			i++
+		}
+		if i == 0 || i+1 >= len(tag) || tag[i] != ':' || tag[i+1] != '"' {
+			break
+		}
+		name := string(tag[:i])
+		tag = tag[i+1:]
+
+		// Scan quoted string to find value.
+		i = 1
+		for i < len(tag) && tag[i] != '"' {
+			if tag[i] == '\\' {
+				i++
+			}
+			i++
+		}
+		if i >= len(tag) {
+			break
+		}
+		qvalue := string(tag[:i+1])
+		tag = tag[i+1:]
+
+		value, err := strconv.Unquote(qvalue)
+		if err != nil {
+			continue
+		}
+
+		tagMap[name] = value
+	}
+
+	return tagMap
 }
 
 // isFieldStringable check if the field type is a scalar. If the field type is
